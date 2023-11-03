72e3fd5c76707a76bbfeb28c06535b0f65635270
Raph <125955477+RaphielSh@users.noreply.github.com>
2023-11-03 17:14:16 +0300

    multi-dimensional len

--- 4Dimensional_dist.cpp
+++ 4Dimensional_dist.cpp
@@ -1,54 +1,118 @@
 #include<iostream>
 #include<cmath>
 #include<iomanip>
+#include<array>
+#include<iterator>
 using namespace std;
 
+template<class T>
 class FourD{
 private:
-	long int x;
-	long int y;
-	long int z;
-	long int k;
+	T x;
+	T y;
+	T z;
+	T k;
 public:
 	FourD(){x = 0.0; y = 0.0; z = 0.0; k = 0.0;}
 	//setter
-	void setPoint(long int a, long int b, long int c,long int d){
+	void setPoint(T a, T b, T c,T d){
 		x = a;
 		y = b;
 		z = c;
 		k = d;
 	}
 	//getters
-	long int getX(){return x;}
-	long int getY(){return y;}
-	long int getZ(){return z;}
-	long int getK(){return k;}
+	T getX(){return x;}
+	T getY(){return y;}
+	T getZ(){return z;}
+	T getK(){return k;}
+
+	template<class U>
+	void Dist(){}
 
 	//print
-	void printPoints(){cout << "\n X = " << x << " Y = " << y << " Z = " << z << " K = " << k << endl;}
+	void printPoints(){cout << setprecision(15);cout << "\n X = " << x << " Y = " << y << " Z = " << z << " K = " << k << endl;}
 };
 
-long long int Square(long int x){return pow(x, 2);}
+template <class T>
+T Square(T x){return pow(x, 2);}
 
-void Dist(FourD object, FourD object2){
+template <class T>
+void Dist(T object, T object2){
 	cout << "Sqrt( (xᵦ - xₐ)² + (yᵦ - yₐ)² + (zᵦ - zₐ)² + (kᵦ - kₐ)² )" << endl;
 	cout << "First point: ";
 	object.printPoints();
 	cout << "Second point: ";
 	object2.printPoints();
-	long int res = sqrt( Square(object2.getX() - object.getX()) + Square(object2.getY() - object.getY()) + Square(object2.getZ() - object.getZ()) + Square(object2.getK() - object.getK()));
+	long double res = sqrt( Square(object2.getX() - object.getX()) + Square(object2.getY() - object.getY()) + Square(object2.getZ() - object.getZ()) + Square(object2.getK() - object.getK()) );
 	cout << "Distance between A and B = " << res << endl;
 }
 
+template<class T, int ARRAY_LEN>
+class NDIM{
+private:
+	int dimension;
+	array<T, ARRAY_LEN> arr;
+public:
+	NDIM(){}
 
+	//setter
+	void setPoint(array<T, ARRAY_LEN> inp_arr){arr = inp_arr;}
+
+	//getter
+
+
+	//print
+	void printPoints(){
+		cout << "\nPoints: \n";
+		for(T i = arr.begin(); i != arr.end(); ++i){
+			cout << "Var " << i << "/" << ARRAY_LEN << " = " << *i;
+		}
+	}
+
+};
 int main(){
 
-	FourD point1;
-	FourD point2;
+	// FourD<double> point1;
+	// FourD<double> point2;
 
-	point1.setPoint(3456, 45723, 45734, 67823);
-	point2.setPoint(34563, 567567, 235346, 45745);
+	int dim = 5;
+	string tmp;
+	int arr_in;
 
-	Dist(point1, point2);
+	// // point1.setPoint(3456.345345, 45723.3646, 45734.56734, 67823.235345);
+	// // point2.setPoint(34563.235346, 567567.457632, 235346.3452, 45745.3644356);
+	// point1.setPoint(3456, 45723, 45734, 67823);
+	// point2.setPoint(34563, 567567, 235346, 45745);
+
+
+	// // point1.printPoints();
+	// // point2.printPoints();
+
+
+	// // cout << point1.getX();
+
+	// Dist(point1, point2);
+
+
+	// cout << "What dimensional you want to use? "; cin >> dim;
+
+	NDIM<int, dim> pnt1;
+	NDIM<int, dim> pnt2;
+
+	array<int, dim> arr;
+	for(int i = 0; i < dim; i++){
+		cout << "Var " << i << "/" << dim << ": ";
+		cin >> arr_in;
+		arr.push_back(arr_in);
+	}
+	pnt1.setPoint(arr);
+	pnt2.setPoint(arr);
+
+	pnt1.printPoints();
+	pnt2.printPoints();
+
+
 	return 0;
 }
+
