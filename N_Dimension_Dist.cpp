5a179d7923d32624c798d0acd255cf30713e4709
Raph <125955477+RaphielSh@users.noreply.github.com>
2023-11-13 21:02:54 +0300

    complete n dimension dist compute

--- N_Dimension_Dist.cpp
+++ N_Dimension_Dist.cpp
@@ -5,49 +5,6 @@
 
 using namespace std;
 
-// template<class T>
-// class FourD{
-// private:
-// 	T x;
-// 	T y;
-// 	T z;
-// 	T k;
-// public:
-// 	FourD(){x = 0.0; y = 0.0; z = 0.0; k = 0.0;}
-// 	//setter
-// 	void setPoint(T a, T b, T c,T d){
-// 		x = a;
-// 		y = b;
-// 		z = c;
-// 		k = d;
-// 	}
-// 	//getters
-// 	T getX(){return x;}
-// 	T getY(){return y;}
-// 	T getZ(){return z;}
-// 	T getK(){return k;}
-
-// 	template<class U>
-// 	void Dist(){}
-
-// 	//print
-// 	void printPoints(){cout << setprecision(15);cout << "\n X = " << x << " Y = " << y << " Z = " << z << " K = " << k << endl;}
-// };
-
-// template <class T>
-// T Square(T x){return pow(x, 2);}
-
-// template <class T>
-// void Dist(T object, T object2){
-// 	cout << "Sqrt( (xᵦ - xₐ)² + (yᵦ - yₐ)² + (zᵦ - zₐ)² + (kᵦ - kₐ)² )" << endl;
-// 	cout << "First point: ";
-// 	object.printPoints();
-// 	cout << "Second point: ";
-// 	object2.printPoints();
-// 	long double res = sqrt( Square(object2.getX() - object.getX()) + Square(object2.getY() - object.getY()) + Square(object2.getZ() - object.getZ()) + Square(object2.getK() - object.getK()) );
-// 	cout << "Distance between A and B = " << res << endl;
-// }
-
 template<typename T>
 class NDIM{
 private:
