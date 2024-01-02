7a7793b79972bccad08e732d39ba7884b9c250ce
NS <wigiglenekit@gmail.com>
2024-01-02 17:25:23 +0300

    hypersphere & hypersphere volume

--- N_Dimension_Dist.cpp
+++ N_Dimension_Dist.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
 #include<cmath>
 #include<iomanip>
-#include <vector>
+#include<vector>
+#include<numbers>
+#include<array>
+#include<format>
+#include<cstdint>
 
+//stupid, fix
 using namespace std;
 
 template<typename T>
@@ -12,9 +17,12 @@ private:
 public:
 	NDIM(){}
 
-	NDIM(NDIM &other):c_vector(other.c_vector){};
+	NDIM(NDIM &other):c_vector(other.c_vector),size(other.size){};
 
-	NDIM(vector<T> &inp_vector):c_vector(inp_vector){};
+	NDIM(vector<T> &inp_vector):c_vector(inp_vector),size(inp_vector.size()){};
+
+	uint64_t size;
+	//int64_t size; // не хочешь сделать математику для отрицательных измерений?)
 
 	//print
 	void printPoints(){
@@ -47,6 +55,57 @@ T Dist(NDIM<T>& obj1, NDIM<T>& obj2){
 		res += pow(*it2-*it1, 2);
 	return sqrt(res);
 }
+
+template<class T>
+struct Hypersphere{
+	NDIM<T> center; //center of the sphere
+	T radius;
+	Hypersphere(NDIM<T> center,T radius):center(center),radius(radius){}
+
+	//Vn=CnR^n
+	//         pi^(n/2)
+	//Cn =  --------------
+	//      gamma((n/2)+1)
+	//TODO:
+	//user-defined pi
+	//user-defined floating point class for more accuracy
+	//user-defined POW
+	//user-defined gammafunc
+	T volume(){
+		return (pow(std::numbers::pi,(static_cast<long double>(center.size)/2))/tgamma((static_cast<long double>(center.size)/2)+1)) 
+		* pow(radius,static_cast<long double>(center.size));
+	}
+
+
+};
+
+//long double
+template<class T>
+bool HypersphereTest(){
+	bool passed = true;
+	//hypesphere in 0,0...0
+	//radius = 1
+	cout << "hypersphere in 0,0...0\nradius=1" << endl;
+	array<long double,20> answers1 = {1, 2, 3.141592654, 4.188790205, 4.934802201,
+	       	5.263789014, 5.167712780, 4.724765970, 4.058712126, 3.298508903, 2.550164040, 1.884103879,
+	       	1.335262769, 0.910628755, 0.599264529, 0.381443281, 0.235330630, 0.140981107, 0.082145887,
+	       	0.046621601};
+	vector<T> init = {};
+	NDIM dims(init);
+	for(int i =0; i<20;i++){
+		NDIM dims(init);
+		Hypersphere<T> s(dims,1.0);
+		//check stupid af
+		//maybe std::numeric_limits<T>::epsilon()?
+		if( abs(s.volume() - answers1[i]) > 0.000000001 ){
+			passed = false;
+			cout << "Error i=" << i << " calc volume: " << s.volume() << " Expected: " <<answers1[i] << endl;
+		}
+		init.push_back(0);
+	}
+	return passed;
+}
+
 
 int main(){
 	size_t dim;
